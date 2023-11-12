64128b9036c353ebc84b998c9df1b35e936a2a6e
ConsoleSystemPrint <github1git@gmail.com>
2023-11-13 02:42:13 +0500

    Initial commit

---
+++ src/main/java/com/example/langbot/LangBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.langbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/langbot/LangBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.langbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
